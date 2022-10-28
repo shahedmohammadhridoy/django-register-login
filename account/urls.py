f9d7f8388771601e1d96a8a4dfcb2df030247fa1
Shahed <shahed_hridoy@outlook.com>
2022-10-28 23:02:11 +0600

    design

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('register', views.index, name='index')
+    path('register', views.index, name='index'),
+    path('login',views.index, name='login')
 ]
 
