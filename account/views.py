cc849857614f80f5c9614dc83dbf65c6962524f5
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2022-10-31 01:11:31 +0600

    logged user verify

--- account/views.py
+++ account/views.py
@@ -6,34 +6,39 @@ from django.contrib.auth.decorators import login_required
 from .forms import CreateUserForm
 
 def registration(request):
-    form = CreateUserForm()
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account created successfully '+ user)
+                return redirect('login')
 
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, 'Account created successfully '+ user)
-            return redirect('login')
 
-
-    context = {
-        'form':form
-    }
-    return render(request, 'registration.html', context)
+        context = {
+            'form':form
+        }
+        return render(request, 'registration.html', context)
     
 def login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            print("Logged In Success")
-            auth_login(request, user)
-            return redirect('dashboard')
-        else:
-            messages.info(request, 'Invalid credential')
-    return render(request, 'login.html')
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                print("Logged In Success")
+                auth_login(request, user)
+                return redirect('dashboard')
+            else:
+                messages.info(request, 'Invalid credential')
+        return render(request, 'login.html')
 
 def logoutUser(request):
     logout(request)
